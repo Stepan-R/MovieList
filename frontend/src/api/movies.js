28fd3510a5fc85902bad0ac80c1a0b58475f98ca
Stepan Rad <stepanrad@MacBook-Air-Stepan.local>
2025-01-14 19:58:32 +0200

    vercel3

--- frontend/src/api/movies.js
+++ frontend/src/api/movies.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = 'http://localhost:3005';
+axios.defaults.baseURL = 'https://movie-list-hpdi.vercel.app/';
 
 export function getAll() {
   return axios.get('/movies')
